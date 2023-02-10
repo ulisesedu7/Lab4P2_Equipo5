f0251cbe46bf1bbf7f541f674edf04df34707777
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-02-10 14:37:05 -0600

    Update main and class Pieza

--- src/lab4p2_equipo5/Lab4P2_Equipo5.java
+++ src/lab4p2_equipo5/Lab4P2_Equipo5.java
@@ -1,8 +1,16 @@
 package lab4p2_equipo5;
 
 public class Lab4P2_Equipo5 {
+    public static Object[][] tablero = new Object[8][8];
+  
 
   public static void main(String[] args) {
+    
+  }
+  
+  public static void crearTablero() {
+    tablero[0][0] = "T";
+    tablero[0][4] = " ";
   }
 
 }
--- src/lab4p2_equipo5/Peon.java
+++ src/lab4p2_equipo5/Peon.java
@@ -1,12 +1,20 @@
 package lab4p2_equipo5;
 
 public class Peon extends Pieza {
+  private boolean comio;
+  
+  
   @Override
   public boolean movimiento(String coordenadaPieza, String coordenadaMov, Object tablero) {
     boolean check = false;
     
     // Validacion del tablero con Peon
-    
+    if(coordenadaPieza.equals(getCoordenadaPieza())) {
+      
+      if (((String)tablero).equals(" ")) {
+        
+      }
+    }
     
     return check;
   }
