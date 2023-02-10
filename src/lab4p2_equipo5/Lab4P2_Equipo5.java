9337a0e1c48edef4eb93887007a157cca69bb963
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2023-02-10 13:40:41 -0600

    Fix main error

--- src/lab4p2_equipo5/Lab4P2_Equipo5.java
+++ src/lab4p2_equipo5/Lab4P2_Equipo5.java
@@ -1,12 +1,8 @@
 package lab4p2_equipo5;
- */
+
 public class Lab4P2_Equipo5 {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
   }
-  
+
 }
